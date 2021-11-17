5f98418b019e91329be15e6347511f4a5f71fd69
zarlishkotwal <you@example.com>
2021-11-17 08:32:49 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,96 @@
+// alert("hello world");
+
+
+// chapter 35 to 38
+
+// Q#1
+
+// function dateandtime(){
+//  document.write(" Current Date and time: "+new Date);
+//  }
+//  dateandtime();
+
+    //  Q#2
+
+//  function fullname(){
+//  var firstname = prompt("Enter your first name");
+// var lastname = prompt("Enter your last name");
+// alert("Full name: "+ firstname + " "+lastname+"\n Welcome to our website!");
+// }
+//     fullname();
+
+    // Q#3
+
+    // function add(){
+    //         var num1 = +prompt("Enter num1 value ");
+    //         var num2 = +prompt("Enter num2 value ");
+    //         var a = num1+num2;
+    //         return a;
+    //          }
+    //          alert("The sum of two numbers is:"+add());
+
+
+    // Q#4
+
+    // function calculator(num1, num2, operator) {
+    //         if (operator === "/")
+    //             return num1 / num2;
+    //         if (operator === "*")
+    //             return num1 * num2;
+    //         if (operator === "+")
+    //             return num1 + num2;
+    //         if (operator === "-")
+    //             return num1 - num2;
+    //         else
+    //             return "Invalid Operator!";
+    //     }
+    //     var num1 = 7;
+    //     var num2 = 7;
+    //     var operator = "+";
+    //     var answer = calculator(num1, num2, operator);
+    //     document.write(`${num1} ${operator} ${num2} = ${answer}`)
+
+// Q#5
+
+    // function getSquare(num) {
+    //         var square = num * num;
+    //         return square;
+    //       }
+    //       var a =+prompt("enter num for square");
+    //       alert("squares of "+ a +" argument is: "+getSquare(a));
+
+
+    // Q#6
+
+    // var a =+prompt("Enter your number for factorial");
+    // var ans = a;
+    // function fact(a){
+    //     if (a==0||a==1){
+    //         return 1;
+    //     }
+    //     else{
+    //         for(var i =a-1;i>=1;i--){
+    //             ans = ans*i;
+    //         }
+    //         return ans;
+    //     }
+    // }
+    // var c = fact(a);
+    // console.log("factorial of number is: "+c);
+ 
+
+    // Q#7
+
+    // function num(start,end){   
+    //         for(var i=start;i<=end;i++){
+    //             document.write(i+"<br>");
+    //         }
+    //     }
+    //     var a=+prompt("enter starting number:");
+    //     var b=+prompt("enter ending number:");
+    //     num(a,b);
+      
+// Q#8
+
+
+
